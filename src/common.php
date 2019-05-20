281ea929241f133406a061def40390df2f660f7b
yupoxiong <8553151@qq.com>
2019-05-20 11:33:49 +0800

    忽略文件

---
+++ src/common.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * 省市区助手函数
+ * @author yupoxiong<i@yufuping.com>
+ */
---
+++ src/controller/Controller.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author yupoxiong<i@yufuping.com>
+ */
+
+namespace yupoxiong\region\controller;
+
+use yupoxiong\region\traits\Result;
+
+class Controller
+{
+    use Result;
+}
